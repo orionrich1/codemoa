35cea1e442ccdd5e8705fa6c920fe5c83b13881f
fodel <kkdd3011@naver.com>
2025-07-11 16:19:35 +0900

    chore: 기능 연습

--- src/main/java/com/codemoa/project/domain/problem/controller/ProblemController.java
+++ src/main/java/com/codemoa/project/domain/problem/controller/ProblemController.java
@@ -1,6 +1,10 @@
 //도영
 package com.codemoa.project.domain.problem.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class ProblemController {
 
 }
