5db799ee83825093c25d2b9778cf7ce3b4c2bb49
lims11 <limjaeyong91@gmail.com>
2025-07-11 18:20:23 +0900

    perf: 몇몇 개선

--- src/main/java/com/codemoa/project/domain/information/controller/InformationController.java
+++ src/main/java/com/codemoa/project/domain/information/controller/InformationController.java
@@ -9,8 +9,20 @@ import lombok.extern.slf4j.Slf4j;
 @Controller
 @Slf4j
 public class InformationController {
+	
 	@GetMapping("/information")
 	public String informationMain() {
 		return "views/information/informationMain";
 	}
+	
+	@GetMapping("/information/detail")
+	public String informationDetail() {
+		return "views/information/informationDetail";
+	}
+	
+	@GetMapping("/information/list")
+	public String informationList() {
+		return "views/information/informationList";
+	}
+	
 }
