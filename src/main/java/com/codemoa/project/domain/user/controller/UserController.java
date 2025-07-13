bab6197f3d7a17ef4b8ba88def5ea63b0560d922
orionrich <orionrich1@gmail.com>
2025-07-13 22:18:59 +0900

    web 컨피그네이션 추가 로그인 작업 중

--- src/main/java/com/codemoa/project/domain/user/controller/UserController.java
+++ src/main/java/com/codemoa/project/domain/user/controller/UserController.java
@@ -1,6 +1,18 @@
 //기찬
 package com.codemoa.project.domain.user.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+import com.springbootstudy.bbs.controller.MemberController;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Controller
+@SessionAttributes("user")
+@Slf4j
 public class UserController {
+	
+	
 
 }
