67e9df614c146b80a2661f781ba37147f19c06c3
orionrich <orionrich1@gmail.com>
2025-07-14 18:20:39 +0900

    25-6-20 로그인 작업중

--- src/main/java/com/codemoa/project/domain/user/controller/UserController.java
+++ src/main/java/com/codemoa/project/domain/user/controller/UserController.java
@@ -4,7 +4,6 @@ package com.codemoa.project.domain.user.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
-import com.springbootstudy.bbs.controller.MemberController;
 
 import lombok.extern.slf4j.Slf4j;
 
