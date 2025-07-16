520ad86423ae0b3f6db77147fa0d18f6a198770b
orionrich <orionrich1@gmail.com>
2025-07-16 18:20:40 +0900

    login 기능 제작중

--- src/main/java/com/codemoa/project/domain/user/controller/UserController.java
+++ src/main/java/com/codemoa/project/domain/user/controller/UserController.java
@@ -2,16 +2,28 @@
 package com.codemoa.project.domain.user.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
+import com.codemoa.project.domain.user.service.UserService;
 
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Controller
 @SessionAttributes("user")
 @Slf4j
+@RequiredArgsConstructor
 public class UserController {
 	
-	
+	 private final UserService userService;
 
+	 
+	 
+	 @PostMapping("/login")
+	    public String login(LoginRequestDto loginDto, HttpSession session) {
+		 
+		 
+	 }
 }
--- src/main/java/com/codemoa/project/domain/user/dto/request/UserLoginRequest.java
+++ src/main/java/com/codemoa/project/domain/user/dto/request/UserLoginRequest.java
@@ -1,6 +1,14 @@
 //기찬 로그인 요청 DTO
 package com.codemoa.project.domain.user.dto.request;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class UserLoginRequest {
+	// 로그인 요청 시 필요한 필드
+	private String userId;
+	private String password;
 
 }
