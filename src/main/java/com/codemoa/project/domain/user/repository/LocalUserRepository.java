b63a337d828f9b8b0393c00b5330ef6e4b387c8e
kimjonghyo <apekcjsrnr@Naver.com>
2025-07-18 15:15:17 +0900

    Revert "main 커밋"
    This reverts commit 1cc096890d64957d87a25cea5612f12b3eb8c9c1.

--- src/main/java/com/codemoa/project/domain/user/repository/LocalUserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.codemoa.project.domain.user.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.codemoa.project.domain.user.entity.LocalUser;
-
-public interface LocalUserRepository extends JpaRepository<LocalUser, String>{
-
-}
