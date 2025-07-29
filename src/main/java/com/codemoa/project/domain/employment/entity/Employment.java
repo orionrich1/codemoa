300658579f573d7a705767e0a2c17c54d3d7fb5c
kimjonghyo <apekcjsrnr@Naver.com>
2025-07-29 14:56:44 +0900

    채용 공고 제작 시작

--- src/main/java/com/codemoa/project/domain/employment/entity/Employment.java
+++ src/main/java/com/codemoa/project/domain/employment/entity/Employment.java
@@ -1,46 +1,36 @@
-//종효
 package com.codemoa.project.domain.employment.entity;
 
+import java.sql.Timestamp;
 import java.time.LocalDateTime;
 
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class Employment {
     
-    private int employmentId;       // employment_id (PK)
-    private String companyName;
-    private String title;
-    private String jobCategory;
-    private String requiredCareer;
-    private String location;
-    private String content;
-    private LocalDateTime dueDate;
-    private LocalDateTime regDate;
-
-    // --- Getters and Setters ---
-    
-    public int getEmploymentId() { return employmentId; }
-    public void setEmploymentId(int employmentId) { this.employmentId = employmentId; }
-
-    public String getCompanyName() { return companyName; }
-    public void setCompanyName(String companyName) { this.companyName = companyName; }
-
-    public String getTitle() { return title; }
-    public void setTitle(String title) { this.title = title; }
-
-    public String getJobCategory() { return jobCategory; }
-    public void setJobCategory(String jobCategory) { this.jobCategory = jobCategory; }
-
-    public String getRequiredCareer() { return requiredCareer; }
-    public void setRequiredCareer(String requiredCareer) { this.requiredCareer = requiredCareer; }
-
-    public String getLocation() { return location; }
-    public void setLocation(String location) { this.location = location; }
-
-    public String getContent() { return content; }
-    public void setContent(String content) { this.content = content; }
-
-    public LocalDateTime getDueDate() { return dueDate; }
-    public void setDueDate(LocalDateTime dueDate) { this.dueDate = dueDate; }
-
-    public LocalDateTime getRegDate() { return regDate; }
-    public void setRegDate(LocalDateTime regDate) { this.regDate = regDate; }
+	@Id
+	private Long recruitNo;	
+	
+	private String insttNm;
+	private String recruitNm;
+	private String jobsCdNm;
+	private String recruitSeCd;
+	private String workRegion;
+	private String career;
+	private Timestamp receprionCloseDt;
+	private String employmentUrl;
+	private Boolean isScraped;
+	
+	@CreationTimestamp
+	private Timestamp createdAt;
+	
 }
+	
--- src/main/java/com/codemoa/project/domain/employment/mapper/EmploymentMapper.java
+++ src/main/java/com/codemoa/project/domain/employment/mapper/EmploymentMapper.java
@@ -6,4 +6,6 @@ import org.apache.ibatis.annotations.Mapper;
 @Mapper
 public interface EmploymentMapper {
 
+	List<Employment> findAll();
+	Emloyment findById(Long recruitNo);
 }
