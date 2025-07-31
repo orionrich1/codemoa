8014009fd970c34d8847c17c043ff86b25c8ce8f
orionrich <orionrich1@gmail.com>
2025-07-31 09:35:38 +0900

    포인트 관련 작업중

--- src/main/java/com/codemoa/project/domain/community/entity/Comment.java
+++ src/main/java/com/codemoa/project/domain/community/entity/Comment.java
@@ -27,42 +27,41 @@ import lombok.NoArgsConstructor;
 @Table(name = "comment")
 public class Comment {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "comment_no")
-    private Integer commentNo;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "comment_no")
+	private Integer commentNo;
 
-    @Lob
-    @Column(nullable = false)
-    private String content;
+	@Lob
+	@Column(nullable = false)
+	private String content;
 
-    @Column(name = "is_adopted")
-    private boolean isAdopted;
+	@Column(name = "is_adopted")
+	private boolean isAdopted;
 
-    @CreationTimestamp
-    @Column(name = "created_at", updatable = false)
-    private LocalDateTime createdAt;
+	@CreationTimestamp
+	@Column(name = "created_at", updatable = false)
+	private LocalDateTime createdAt;
 
-    // --- 연관 관계 ---
+	// --- 연관 관계 ---
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id") // 댓글 작성자
-    private User user;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id") // 댓글 작성자
+	private User user;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "board_no") // 댓글이 달린 게시글
-    private CommunityBoard communityBoard;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "board_no") // 댓글이 달린 게시글
+	private CommunityBoard communityBoard;
 
+	@Builder
+	public Comment(String content, boolean isAdopted, User user, CommunityBoard communityBoard) {
+		this.content = content;
+		this.isAdopted = isAdopted;
+		this.user = user;
+		this.communityBoard = communityBoard;
+	}
 
-    @Builder
-    public Comment(String content, boolean isAdopted, User user, CommunityBoard communityBoard) {
-        this.content = content;
-        this.isAdopted = isAdopted;
-        this.user = user;
-        this.communityBoard = communityBoard;
-    }
-    
-    public void adopt() {
-        this.isAdopted = true;
-    }
+	public void adopt() {
+		this.isAdopted = true;
+	}
 }
